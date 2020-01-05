212a8eca23b7cdccc46c697628c9ac3f3b9ed35a
usdrd <aqlsz@naver.com>
2020-01-05 19:44:57 +0900

    DB연결

---
+++ Spring_blog/src/test/java/org/woo/web/DBTest.java
@@ -0,0 +1,50 @@
+package org.woo.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+public class DBTest {
+	
+	@Test
+	public void OrcleDBConnectionTest() throws Exception {
+	Class.forName("oracle.jdbc.driver.OracleDriver");
+	Connection con = DriverManager.getConnection(
+	"jdbc:oracle:thin:@192.168.0.33:1521:orcl",
+	"orcl",
+	"1234");
+	System.out.println(con);
+	con.close();
+	}
+
+	@Test
+	public void postgresDBConnectionTest() throws Exception {
+	Class.forName("org.postgresql.Driver");
+	Connection con = DriverManager.getConnection(
+	"jdbc:postgresql://localhost:5432/postgres",
+	"postgres",
+	"1234");
+	System.out.println(con);
+	con.close();
+	}
+	
+	@Test
+	public void MysqlDBConnectionTest() throws Exception {
+    //mysql driver name 5version
+	//com.mysql.jdbc.Driver
+	//8버전 부터 driver name이 변경 되었고 Timezone을 설정하도록 변경 되었다.
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	Connection con = DriverManager.getConnection(
+	"jdbc:mysql://localhost:3306/blog?useSSL=false&serverTimezone=Asia/Seoul",
+	"blog",
+	"1234");
+	System.out.println(con);
+	con.close();
+	}
+}
